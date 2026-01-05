d55b0dca6bfa51e847a8964a4756a0b175990eb1
andrew003 <timosha003@ukr.net>
2026-01-05 14:21:01 +0200

    #2 - code refactor

--- cypress/e2e/calculator.spec.cy.js
+++ cypress/e2e/calculator.spec.cy.js
@@ -1,128 +1,173 @@
 describe('e2e test for calculator app', () => {
 
+  const clickButton = (buttonName) => {
+    cy.get(`[data-test="${buttonName}"]`).click();
+  };
+
+  const clickDigit = (digit) => {
+    clickButton(`btn-${digit}`);
+  };
+
+  const clickOperator = (operator) => {
+    const operatorMap = {
+      '+': 'btn-plus',
+      '-': 'btn-minus',
+      '*': 'btn-multiply',
+      '/': 'btn-slash',
+      '=': 'btn-equal',
+      '.': 'btn-dot',
+      '%': 'btn-percentages'
+    };
+    clickButton(operatorMap[operator]);
+  };
+
+  const getDashboardValue = () => {
+    return cy.get('[data-test="dashboard"]');
+  };
+
+  const clearCalculator = () => {
+    clickButton('btn-clr');
+  };
+
+  const togglePlusMinus = () => {
+    clickButton('btn-plus-minus');
+  };
+
+  const saveResult = () => {
+    clickButton('btn-save');
+  };
+
+  const pasteResult = () => {
+    clickButton('btn-paste');
+  };
+
+  const toggleTheme = () => {
+    clickButton('btn-theme');
+  };
+
   before(() => {
     cy.visit('http://localhost:3000/');
   });
 
   beforeEach(() => {
-    cy.get('[data-test="btn-clr"]').click();
+    clearCalculator();
   });
 
   it('should check expression 2+9-6', () => {
-    cy.get('[data-test="btn-2"]').click();
-    cy.get('[data-test="btn-plus"]').click();
-    cy.get('[data-test="btn-9"]').click();
-    cy.get('[data-test="btn-minus"]').click();
-    cy.get('[data-test="btn-6"]').click();
+    clickDigit(2);
+    clickOperator('+');
+    clickDigit(9);
+    clickOperator('-');
+    clickDigit(6);
 
-    cy.get('[data-test="dashboard"]').should('have.value', '2+9-6');
+    getDashboardValue().should('have.value', '2+9-6');
   })
 
   it('should check result of expression 2+9-6', () => {
-    cy.get('[data-test="btn-2"]').click();
-    cy.get('[data-test="btn-plus"]').click();
-    cy.get('[data-test="btn-9"]').click();
-    cy.get('[data-test="btn-minus"]').click();
-    cy.get('[data-test="btn-6"]').click();
-    cy.get('[data-test="btn-equal"]').click();
+    clickDigit(2);
+    clickOperator('+');
+    clickDigit(9);
+    clickOperator('-');
+    clickDigit(6);
+    clickOperator('=');
 
-    cy.get('[data-test="dashboard"]').should('have.value', '5');
+    getDashboardValue().should('have.value', '5');
   })
   it('should check result of expression 1+2+3+4-5-6-7-8-9', () => {
-    cy.get('[data-test="btn-1"]').click();
-    cy.get('[data-test="btn-plus"]').click();
-    cy.get('[data-test="btn-2"]').click();
-    cy.get('[data-test="btn-plus"]').click();
-    cy.get('[data-test="btn-3"]').click();
-    cy.get('[data-test="btn-plus"]').click();
+    clickDigit(1);
+    clickOperator('+');
+    clickDigit(2);
+    clickOperator('+');
+    clickDigit(3);
+    clickOperator('+');
     cy.contains('button', '4').click();
-    cy.get('[data-test="btn-minus"]').click();
+    clickOperator('-');
     cy.contains('button', '5').click();
-    cy.get('[data-test="btn-minus"]').click();
-    cy.get('[data-test="btn-6"]').click();
-    cy.get('[data-test="btn-minus"]').click();
+    clickOperator('-');
+    clickDigit(6);
+    clickOperator('-');
     cy.contains('button', '7').click();
-    cy.get('[data-test="btn-minus"]').click();
+    clickOperator('-');
     cy.contains('button', '8').click();
-    cy.get('[data-test="btn-minus"]').click();
-    cy.get('[data-test="btn-9"]').click();
-    cy.get('[data-test="btn-equal"]').click();
+    clickOperator('-');
+    clickDigit(9);
+    clickOperator('=');
 
-    cy.get('[data-test="dashboard"]').should('have.value', '-25');
+    getDashboardValue().should('have.value', '-25');
   })
   it('should check result expression 10/10', () => {
-    cy.get('[data-test="btn-1"]').click();
-    cy.get('[data-test="btn-0"]').click();
-    cy.get('[data-test="btn-slash"]').click();
-    cy.get('[data-test="btn-1"]').click();
-    cy.get('[data-test="btn-0"]').click();
-    cy.get('[data-test="btn-equal"]').click();
+    clickDigit(1);
+    clickDigit(0);
+    clickOperator('/');
+    clickDigit(1);
+    clickDigit(0);
+    clickOperator('=');
 
-    cy.get('[data-test="dashboard"]').should('have.value', '1');
+    getDashboardValue().should('have.value', '1');
   })
 
   it('should check result expression 0.5*10', () => {
-    cy.get('[data-test="btn-0"]').click();
-    cy.get('[data-test="btn-dot"]').click();
+    clickDigit(0);
+    clickOperator('.');
     cy.contains('button', '5').click();
-    cy.get('[data-test="btn-multiply"]').click();
-    cy.get('[data-test="btn-1"]').click();
-    cy.get('[data-test="btn-0"]').click();
-    cy.get('[data-test="btn-equal"]').click();
+    clickOperator('*');
+    clickDigit(1);
+    clickDigit(0);
+    clickOperator('=');
 
-    cy.get('[data-test="dashboard"]').should('have.value', '5');
+    getDashboardValue().should('have.value', '5');
   })
 
   it('should check result expression -0.1*99', () => {
-    cy.get('[data-test="btn-0"]').click();
-    cy.get('[data-test="btn-dot"]').click();
-    cy.get('[data-test="btn-1"]').click();
-    cy.get('[data-test="btn-plus-minus"]').click();
-    cy.get('[data-test="btn-multiply"]').click();
-    cy.get('[data-test="btn-9"]').click();
-    cy.get('[data-test="btn-9"]').click();
-    cy.get('[data-test="btn-equal"]').click();
+    clickDigit(0);
+    clickOperator('.');
+    clickDigit(1);
+    togglePlusMinus();
+    clickOperator('*');
+    clickDigit(9);
+    clickDigit(9);
+    clickOperator('=');
 
-    cy.get('[data-test="dashboard"]').should('have.value', '-9.9');
+    getDashboardValue().should('have.value', '-9.9');
   })
 
   it('should calculate 10 percent of 1,000', () => {
-    cy.get('[data-test="btn-1"]').click();
-    cy.get('[data-test="btn-0"]').click();
-    cy.get('[data-test="btn-0"]').click();
-    cy.get('[data-test="btn-0"]').click();
-    cy.get('[data-test="btn-multiply"]').click();
-    cy.get('[data-test="btn-1"]').click();
-    cy.get('[data-test="btn-0"]').click();
-    cy.get('[data-test="btn-percentages"]').click();
-    cy.get('[data-test="btn-equal"]').click();
+    clickDigit(1);
+    clickDigit(0);
+    clickDigit(0);
+    clickDigit(0);
+    clickOperator('*');
+    clickDigit(1);
+    clickDigit(0);
+    clickOperator('%');
+    clickOperator('=');
 
-    cy.get('[data-test="dashboard"]').should('have.value', '100');
+    getDashboardValue().should('have.value', '100');
   })
 
   it('should check clear button', () => {
-    cy.get('[data-test="btn-1"]').click();
-    cy.get('[data-test="btn-2"]').click();
-    cy.get('[data-test="btn-3"]').click();
-    cy.get('[data-test="btn-plus"]').click();
-    cy.get('[data-test="btn-9"]').click();
+    clickDigit(1);
+    clickDigit(2);
+    clickDigit(3);
+    clickOperator('+');
+    clickDigit(9);
 
-    cy.get('[data-test="dashboard"]').should('not.have.value', '');
+    getDashboardValue().should('not.have.value', '');
 
-    cy.get('[data-test="btn-clr"]').click();
+    clearCalculator();
 
-    cy.get('[data-test="dashboard"]').should('have.value', '');
+    getDashboardValue().should('have.value', '');
   })
 
   it('should check save button', () => {
-    cy.get('[data-test="btn-2"]').click();
-    cy.get('[data-test="btn-plus"]').click();
-    cy.get('[data-test="btn-3"]').click();
-    cy.get('[data-test="btn-equal"]').click();
+    clickDigit(2);
+    clickOperator('+');
+    clickDigit(3);
+    clickOperator('=');
 
-    cy.get('[data-test="dashboard"]').should('have.value', '5');
+    getDashboardValue().should('have.value', '5');
 
-    cy.get('[data-test="btn-save"]').click();
+    saveResult();
 
     cy.window().then((win) => {
       const savedValue = win.localStorage.getItem('result');
@@ -131,17 +176,17 @@ describe('e2e test for calculator app', () => {
   })
 
   it('should check paste button', () => {
-    cy.get('[data-test="btn-9"]').click();
-    cy.get('[data-test="btn-9"]').click();
-    cy.get('[data-test="btn-save"]').click();
+    clickDigit(9);
+    clickDigit(9);
+    saveResult();
 
-    cy.get('[data-test="btn-clr"]').click();
+    clearCalculator();
 
-    cy.get('[data-test="dashboard"]').should('have.value', '');
+    getDashboardValue().should('have.value', '');
 
-    cy.get('[data-test="btn-paste"]').click();
+    pasteResult();
 
-    cy.get('[data-test="dashboard"]').should('have.value', '99');
+    getDashboardValue().should('have.value', '99');
   })
 
   it('should check change theme, toggle theme button', () => {
@@ -152,12 +197,12 @@ describe('e2e test for calculator app', () => {
 
     cy.get('body').should('have.class', 'theme-one');
 
-    cy.get('[data-test="btn-theme"]').click();
+    toggleTheme();
     cy.wait(600);
 
     cy.get('body').should('have.class', 'theme-second');
 
-    cy.get('[data-test="btn-theme"]').click();
+    toggleTheme();
     cy.wait(600);
 
     cy.get('body').should('have.class', 'theme-one');
